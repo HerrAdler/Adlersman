f4e790c6f719183187a69b5e1bb47c6c7a1c8495
HerrAdler <lucaeigenbrod2008@gmail.com>
2024-02-22 12:57:05 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -41,7 +41,7 @@ Config.announcementLength = 3000;
  * Image Filename
  * DROP IMAGE IN "images" FOLDER
  */
-Config.backgroundImage = "streamm";
+Config.backgroundImage = "";
 
 /**
  * Enable debug messages?
