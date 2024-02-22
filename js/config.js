f15ffc38dc76678e01c5bd82495d7217cc0344f2
HerrAdler <121702828+HerrAdler@users.noreply.github.com>
2024-02-22 13:34:26 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -41,7 +41,7 @@ Config.announcementLength = 3000;
  * Image Filename
  * DROP IMAGE IN "images" FOLDER
  */
-Config.backgroundImage = "diemauer1.jpg";
+Config.backgroundImage = "default", "diemauer1.jpg";
 
 /**
  * Enable debug messages?
